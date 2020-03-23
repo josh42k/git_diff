02a031808b0390f3bfebe34689653674fccf839f
Josh Kitchen <45073406+josh42k@users.noreply.github.com>
2020-03-23 13:50:51 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git
+{
+    class Git
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static bool Diff(string file1, string file2)
+        {
+            if (file1 == file2) return true;
+            else return false;
+        }
+    }
+}
